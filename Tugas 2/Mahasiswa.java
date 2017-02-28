c54908392a09774597d44120244100dbe8f16b79
fjarnugraha <fjarnugraha763@gmail.com>
2017-02-28 19:40:40 +0700

    tugas 2

---
+++ Tugas 2/Mahasiswa.java
@@ -0,0 +1,8 @@
+class Mahasiswa{
+   public static void main(String[] args) {
+        System.out.println("Nama   : Fajar Nugraha Sugiarto");
+        System.out.println("Nim    : 10115339");
+        System.out.println("Alamat : Jl.Dipatiukur , Bandung");
+    }
+    
+}

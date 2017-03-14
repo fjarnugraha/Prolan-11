a10ce771d239dba6f364ff8dd9ea30c9256f136f
fjarnugraha <fjarnugraha763@gmail.com>
2017-03-14 21:52:34 +0700

    Add files via upload

---
+++ Tugas 5 Tipe Data/Perusahaan.java
@@ -0,0 +1,25 @@
+class Perusahaan { 
+  	public static void main (String[] args){
+  		Manager atasan = new Manager();
+  		Karyawan data  =  new Karyawan();
+
+		//data Manager
+		atasan.id      = "A010";
+  		atasan.nama    = "Fajar Nugraha Sugiarto";
+  		atasan.jabatan = "Manager Programmer";
+  		atasan.alamat  = "JL.Dipatiukur,Bandung";
+
+  		//data keluaran Manager.java
+  		atasan.Managerr();
+  		System.out.println("\tID                   = "+atasan.id);
+  		System.out.println("\tNama Atasan          = "+atasan.nama);
+  		System.out.println("\tJabatan              = "+atasan.jabatan);
+  		System.out.println("\tAlamat               = "+atasan.alamat);
+  		
+  		//data keluaran Karyawan.java 
+  		data.judul();
+  		data.psn();
+		System.out.println("\t---------------------------------------------------------");
+
+  	}
+ }
